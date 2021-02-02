ff62ce0b0d123f2474b2ad3dc7afb055415c5021
chisato.kawakami <hanachi102106@gmail.com>
2021-02-02 02:39:59 +0000

    for heroku6

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,7 @@ class TasksController < ApplicationController
   #ログイン検証の処理　require
   #ログインした人が、タスクの持ち主なのか コレクトユーザ追加correct_user
   before_action :set_task, only: [:show, :edit, :update, :destroy]
-  before_action :require_user_logged_in, only: [:index, :show]
+  before_action :require_user_logged_in, only: [:index, :show, :edit]
   before_action :correct_user, only: [:show, :edit, :update, :destroy]
   
   def index
@@ -55,8 +55,9 @@ class TasksController < ApplicationController
   private
   
   def set_task
+    @task = Task.find(params[:id])
   end
-
+  
   def task_params
     params.require(:task).permit(:content, :status)
   end
@@ -65,12 +66,6 @@ class TasksController < ApplicationController
     @task = current_user.tasks.find_by(id: params[:id])
     unless @task
       redirect_to root_url
-    end
-  end
-  
-  def require_user_logged_in
-    unless logged_in?
-      redirect_to login_url
     end
   end
 end
